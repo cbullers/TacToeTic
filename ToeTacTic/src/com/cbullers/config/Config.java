fc4fee034c34e39995680a3ed5debd163977b2e3
Chris Bullers <chrisbullers@gmail.com>
2016-05-04 15:25:34 -0500

    splash screen created
    more to come

---
+++ ToeTacTic/src/com/cbullers/config/Config.java
@@ -0,0 +1,16 @@
+package com.cbullers.config;
+
+import java.awt.Color;
+
+public class Config {
+
+	/*
+	 * Variable class, with all the crap
+	 */
+	
+	// Splash screen
+	public static Color SPLASH_BACKGROUND = Color.BLUE;
+	
+	public static String APP_NAME = "ToeTacTic";
+	
+}
